ecf2e59893dfc13841a74443eec41b9ac707e6a4
Benito GR <bgr.online@posteo.net>
2021-08-20 18:42:18 +0200

    add redux and react-router

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,18 +1,47 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter, Link, Route, Switch } from "react-router-dom";
+import { Counter } from './components/counter';
+import store from './store';
 
 interface AppProps {
-    message: string;
+  message: string;
 }
 
 class App extends React.Component<AppProps> {
-    render() {
-        return <h2>{this.props.message}</h2>
-    }
+  render() {
+    return (
+      <HashRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <h2>💖 Welcome!</h2>
+            <p>Welcome to your Electron application.</p>
+          </Route>
+          <Route path="/counter" component={Counter} />
+        </Switch>
+      </HashRouter>
+    )
+  }
 }
 
 function render() {
-  ReactDOM.render(<App message="Hello from React!" />, document.getElementById('app'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App message="Hello from React!" />
+    </Provider>,
+    document.getElementById('app')
+  );
 }
 
 render();
---
+++ src/ui/components/counter.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useAppSelector, useAppDispatch } from '../store';
+import { decrement, increment } from '../store/counter';
+
+export const Counter = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
